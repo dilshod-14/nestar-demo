7e8bf888c2d24aea9fe06fe75dfe1c88f9ec17a3
dilshod-14 <urolovdilshod24@gmail.com>
2025-07-09 23:56:52 +0900

    feat: publish  homepage screen component

--- libs/components/layout/LayoutHome.tsx
+++ libs/components/layout/LayoutHome.tsx
@@ -4,6 +4,10 @@ import Top from "../Top";
 import Footer from "../Footer";
 import HeaderFilter from "../homepage/HeaderFilter";
 
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
 const withLayoutMain = (Component: any) => {
   return (props: any) => {
     return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,31 @@
-import withLayoutMain from "@/libs/components/layout/LayoutHome";
+// import Advertisement from "@/libs/components/homepage/Advertisement";
+// import PopularProperties from "@/libs/components/homepage/PopularProperties";
+// import TopAgents from "@/libs/components/homepage/TopAgents";
+// import TopProperties from "@/libs/components/homepage/TopProperties";
+// import TrendProperties from "@/libs/components/homepage/TrendProperties";
+// import withLayoutMain from "@/libs/components/layout/LayoutHome";
+
 import { Stack } from "@mui/material";
 import { NextPage } from "next";
 
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import TrendProperties from "@/libs/components/homepage/TrendProperties";
+import PopularProperties from "@/libs/components/homepage/PopularProperties";
+import Advertisement from "@/libs/components/homepage/Advertisement";
+import TopProperties from "@/libs/components/homepage/TopProperties";
+import TopAgents from "@/libs/components/homepage/TopAgents";
+import withLayoutMain from "@/libs/components/layout/LayoutHome";
+
 const Home: NextPage = () => {
   return (
-    <Stack className={"home-page"}>
-      <Stack>
-        <Stack>
-          <Stack className="container">Trend Properties</Stack>
-        </Stack>
-        <Stack>
-          <Stack className="container">Popular Properties</Stack>
-        </Stack>
-        <Stack>
-          <Stack className="container">Edvertisement</Stack>
-        </Stack>
-
-        <Stack></Stack>
-        <Stack className="container">Top Properties</Stack>
-      </Stack>
-      <Stack>
-        <Stack className="container">Top Agents</Stack>
-      </Stack>
+    <Stack className="home-page">
+      <TrendProperties />
+      <PopularProperties />
+      <Advertisement />
+      <TopProperties />
+      <TopAgents />
     </Stack>
   );
 };
